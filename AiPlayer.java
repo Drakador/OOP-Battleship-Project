b0d04e582ab2b8b1b8d771cc9942fb2afdd5d551
kimhisson <61773655+kimhisson@users.noreply.github.com>
2021-04-25 04:31:36 -0500

    Add files via upload

---
+++ AiPlayer.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class AiPlayer extends Player{
+    private String name = "";
+    // Sets player up as a Computer player as opposed to a human player
+    @Override
+    public void setName(String name) {
+        this.name = "Machine Player";
+    }
+
+    // Overrides and sets  isHuman boolean to False
+    @Override
+    public boolean isHuman() {
+        return false;
+    }
+
+
+}
