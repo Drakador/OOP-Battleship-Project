0b4ddf55a7f4cc710a523497b7aebcebf0f24ecf
AaronSkone <81197891+AaronSkone@users.noreply.github.com>
2021-04-28 18:55:47 -0500

    Update Main.java
    Program is currently running but is missing a few key things for user ease. User input validation for the input integer is also causing the program to crash, despite having a try/catch put in place to allow the user to continue. Same method used for input string, which works just fine.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,20 +10,81 @@
 
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// Board test
-        Board ai = new Board(false);
-        ai.printBoard();
-        System.out.println("--------------------------------");
-        Board b = new Board(true);
-        b.printBoard();
 
-        b.attack("A",1);
-        b.printBoard();
-        System.out.println("--------------------------------");
-        ai.attack("a",1);
-        ai.printBoard();
+    public static void main(String[] args) {
+        String xCoordinate = "";
+        int aiHits = 0;
+        int yCoordinate = 0;
+        Boolean isHit;
+
+        Scanner scan = new Scanner(System.in);
+
+        Player newPlayer = new Player();
+        newPlayer.getName();
+        newPlayer.setName(newPlayer.name);
+
+        Machine ai = new Machine();
+        ai.shotTracker();
+
+
+        Board aiBoard = new Board(false);
+        Board playerBoard = new Board(true);
+
+        System.out.println("** Welcome to Battleship **");
+
+        while(aiBoard.checkWin() == false && playerBoard.checkWin() == false){
+            aiBoard.printBoard();
+            System.out.println("____________________________");
+            playerBoard.printBoard();
+            System.out.println("Your move, " + newPlayer.name);
+            try{
+                System.out.println("Enter an X coordinate.");
+                 xCoordinate = scan.next();
+            }
+
+            catch(InputMismatchException exception){
+                System.out.println("This is not a valid X coordinate, try again.");
+                xCoordinate = scan.next();
+            }
+
+            while (newPlayer.validLetter(xCoordinate.toUpperCase()) == false) {
+                System.out.println("That is not a valid X coordinate, try again.");
+                xCoordinate = scan.next();
+            }
+
+            try{
+                System.out.println("Enter a Y coordinate.");
+                yCoordinate = scan.nextInt();
+
+            }
+
+            catch(InputMismatchException exception){
+                System.out.println("This is not a valid Y coordinate, try again.");
+                yCoordinate = scan.nextInt();
+            }
+
+            while (newPlayer.validNumber(yCoordinate) == false) {
+                System.out.println("This is not a valid Y coordinate, try again.");
+                yCoordinate = scan.nextInt();
+            }
+
+            aiBoard.attack(xCoordinate, yCoordinate - 1);
+            
+            System.out.println("The computer fires. ");
+            isHit = ai.fireAtHumanShips(ai.shotTracker(), playerBoard);
+            ai.logShot(ai.shotTracker(), );
+		
+        }
+
+
+
+
     }
+
 }
+
