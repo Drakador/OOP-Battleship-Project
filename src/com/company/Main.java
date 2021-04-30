67d517a005973b0daccf812f671cbcaabc5a7b69
AaronSkone <81197891+AaronSkone@users.noreply.github.com>
2021-04-29 19:04:49 -0500

    Update Main.java
    Integer validation no longer causes program to crash if string is input. Program still crashes if an out of range number is input, however. Sunken ships are now displayed.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@
 
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -31,23 +33,26 @@ public class Main {
         Machine ai = new Machine();
         ai.shotTracker();
 
-
         Board aiBoard = new Board(false);
+        aiBoard.getPieces();
+        ArrayList<Piece> aiPieces = aiBoard.getPieces();
         Board playerBoard = new Board(true);
+        playerBoard.getPieces();
+        ArrayList<Piece> playerPieces = playerBoard.getPieces();
+        playerBoard.getPieces();
 
         System.out.println("** Welcome to Battleship **");
 
-        while(aiBoard.checkWin() == false && playerBoard.checkWin() == false){
+        while (aiBoard.checkWin() == false && playerBoard.checkWin() == false) {
             aiBoard.printBoard();
             System.out.println("____________________________");
             playerBoard.printBoard();
             System.out.println("Your move, " + newPlayer.name);
-            try{
-                System.out.println("Enter an X coordinate.");
-                 xCoordinate = scan.next();
-            }
 
-            catch(InputMismatchException exception){
+            try {
+                System.out.println("Enter an X coordinate A-J.");
+                xCoordinate = scan.next();
+            } catch (InputMismatchException exception) {
                 System.out.println("This is not a valid X coordinate, try again.");
                 xCoordinate = scan.next();
             }
@@ -56,34 +61,43 @@ public class Main {
                 System.out.println("That is not a valid X coordinate, try again.");
                 xCoordinate = scan.next();
             }
+            System.out.println("Enter a Y coordinate 1-10.");
 
-            try{
-                System.out.println("Enter a Y coordinate.");
-                yCoordinate = scan.nextInt();
+                try {
+                    yCoordinate = scan.nextInt();
+                }
+                catch (InputMismatchException exception) {
+                    while (!scan.hasNextInt()) {
+                        scan.next();
+                        System.out.println("This is not a valid Y coordinate, try again.");
+                    }
+                    yCoordinate = scan.nextInt();
+                }
 
+
+                aiBoard.attack(xCoordinate, yCoordinate - 1);
+
+                for(int i = 0; i < aiPieces.size(); i++){
+                    if(aiPieces.get(i).isSunk()){
+                        System.out.println("Enemy " + aiPieces.get(i) + " is sunk!");
+                    }
+                }
+
+                System.out.println("The computer fires. ");
+                isHit = ai.fireAtHumanShips(ai.shotTracker(), playerBoard);
+
+                for(int i = 0; i < playerPieces.size(); i++){
+                    if(playerPieces.get(i).isSunk()){
+                        System.out.println("Friendly " + playerPieces.get(i) + " is sunk!");
+                    }
+                }
             }
 
-            catch(InputMismatchException exception){
-                System.out.println("This is not a valid Y coordinate, try again.");
-                yCoordinate = scan.nextInt();
-            }
 
-            while (newPlayer.validNumber(yCoordinate) == false) {
-                System.out.println("This is not a valid Y coordinate, try again.");
-                yCoordinate = scan.nextInt();
-            }
-
-            aiBoard.attack(xCoordinate, yCoordinate - 1);
-            
-            System.out.println("The computer fires. ");
-            isHit = ai.fireAtHumanShips(ai.shotTracker(), playerBoard);
-		
         }
-
-
-
 
     }
 
-}
+
+
 
