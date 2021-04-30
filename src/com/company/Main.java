98413f04b4f18d8875ebf8f623ba12ca774f3fc9
AaronSkone <81197891+AaronSkone@users.noreply.github.com>
2021-04-30 15:20:13 -0500

    Update Main.java
    Fixed integer input validation, now allows user to keep trying if out of range integer is input as well as if a string is input. General tidying up.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,6 @@
 
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -19,31 +18,27 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String xCoordinate = "";
-        int aiHits = 0;
+        String xCoordinate;
         int yCoordinate = 0;
         Boolean isHit;
 
         Scanner scan = new Scanner(System.in);
 
         Player newPlayer = new Player();
-        newPlayer.getName();
         newPlayer.setName(newPlayer.name);
 
         Machine ai = new Machine();
         ai.shotTracker();
 
         Board aiBoard = new Board(false);
-        aiBoard.getPieces();
         ArrayList<Piece> aiPieces = aiBoard.getPieces();
+
         Board playerBoard = new Board(true);
-        playerBoard.getPieces();
         ArrayList<Piece> playerPieces = playerBoard.getPieces();
-        playerBoard.getPieces();
 
         System.out.println("** Welcome to Battleship **");
 
-        while (aiBoard.checkWin() == false && playerBoard.checkWin() == false) {
+        while (!aiBoard.checkWin() && !playerBoard.checkWin()) {
             aiBoard.printBoard();
             System.out.println("____________________________");
             playerBoard.printBoard();
@@ -52,52 +47,58 @@ public class Main {
             try {
                 System.out.println("Enter an X coordinate A-J.");
                 xCoordinate = scan.next();
-            } catch (InputMismatchException exception) {
+            } 
+            
+            catch (InputMismatchException exception) {
                 System.out.println("This is not a valid X coordinate, try again.");
                 xCoordinate = scan.next();
             }
 
-            while (newPlayer.validLetter(xCoordinate.toUpperCase()) == false) {
+            while (!newPlayer.validLetter(xCoordinate.toUpperCase())) {
                 System.out.println("That is not a valid X coordinate, try again.");
                 xCoordinate = scan.next();
             }
+            
             System.out.println("Enter a Y coordinate 1-10.");
 
-                try {
+            try {
+                yCoordinate = scan.nextInt();
+                while (!newPlayer.validNumber(yCoordinate)) {
+                    System.out.println("This is not a valid Y coordinate, try again.");
                     yCoordinate = scan.nextInt();
                 }
-                catch (InputMismatchException exception) {
-                    while (!scan.hasNextInt()) {
-                        scan.next();
-                        System.out.println("This is not a valid Y coordinate, try again.");
-                    }
-                    yCoordinate = scan.nextInt();
+            }
+            
+            catch (InputMismatchException exception) {
+                while (!scan.hasNextInt()) {
+                    scan.next();
+                    System.out.println("This is not a valid Y coordinate, try again.");
                 }
+                yCoordinate = scan.nextInt();
+            }
+            
+            aiBoard.attack(xCoordinate, yCoordinate - 1);
 
-
-                aiBoard.attack(xCoordinate, yCoordinate - 1);
-
-                for(int i = 0; i < aiPieces.size(); i++){
-                    if(aiPieces.get(i).isSunk()){
-                        System.out.println("Enemy " + aiPieces.get(i).type + " is sunk!");
-                    }
-                }
-
-                System.out.println("The computer fires. ");
-                isHit = ai.fireAtHumanShips(ai.shotTracker(), playerBoard);
-
-                for(int i = 0; i < playerPieces.size(); i++){
-                    if(playerPieces.get(i).isSunk()){
-                        System.out.println("Friendly " + playerPieces.get(i).type + " is sunk!");
-                    }
+            for(int i = 0; i < aiPieces.size(); i++){
+                if(aiPieces.get(i).isSunk()){
+                    System.out.println("Enemy " + aiPieces.get(i) + " is sunk!");
                 }
             }
 
+            System.out.println("The computer fires. ");
+            isHit = ai.fireAtHumanShips(ai.shotTracker(), playerBoard);
+
+            for(int i = 0; i < playerPieces.size(); i++){
+                if(playerPieces.get(i).isSunk()){
+                    System.out.println("Friendly " + playerPieces.get(i).type + " is sunk!");
+                }
+            }
 
         }
 
     }
 
+}
 
 
 
